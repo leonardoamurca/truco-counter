cf7297450124230c5c6d5e505369e65b7a5e907a
leonardoamurca <leonardoamurca@gmail.com>
2019-01-30 14:56:07 -0200

    Subindo nível dos arquivos

---
+++ build/precache-manifest.6889ba00fec0f9c317a1e2a75a7b2716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7efdfcbf246d5e73b44ce51ce9f81b05",
+    "url": "/static/media/truco_logo.7efdfcbf.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32dabb5569a5fa0800f1",
+    "url": "/static/js/main.32dabb55.chunk.js"
+  },
+  {
+    "revision": "0346ef5066c3bf4cfdd1",
+    "url": "/static/js/1.0346ef50.chunk.js"
+  },
+  {
+    "revision": "32dabb5569a5fa0800f1",
+    "url": "/static/css/main.c704a0cf.chunk.css"
+  },
+  {
+    "revision": "40fc40384e601a1892bcc937f465cbe6",
+    "url": "/index.html"
+  }
+];
